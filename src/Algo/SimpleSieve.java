2262685966504f9a44c41479e4835e5c1548525b
Aditya Joshi <aditya03joshi@gmail.com>
2024-02-14 13:05:03 +0530

    java-patrick

--- src/Algo/SimpleSieve.java
+++ src/Algo/SimpleSieve.java
@@ -1,7 +1,32 @@
+/*
+this algo is used to find the prime numbers
+ */
+
 package Algo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SimpleSieve {
     public static void main(String[] args) {
+        int n = 50;
+        boolean[] x = new boolean[n+1];
+        for (int i = 2; i<=Math.sqrt(n);i++) {
+            if(x[i]!= true) {
+                for (int j = i*i; j <=n ; j=j+i) {
+                    x[j]=true;
+                }
+            }
+        }
+        int count = 0 ;
+        for (int i = 2; i <=n; i++) {
+            if(x[i]!=true) {
+                System.out.println(i);
+                count ++ ;
+            }
+        }
+        System.out.println("total prime"+ " " + count);
+
 
     }
 }
