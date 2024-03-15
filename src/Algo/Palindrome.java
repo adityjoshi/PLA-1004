d7ea61d1bf7559e3873f294cdb58d45c7cd828d5
Aditya Joshi <aditya03joshi@gmail.com>
2024-03-15 12:17:22 +0530

    java

--- src/Algo/Palindrome.java
+++ src/Algo/Palindrome.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-
+        System.out.println(pal(8));
+    }
+    static boolean pal(int n) {
+        String s  = Integer.toBinaryString(n);
+        int i = 0 ;
+        int j = s.length()-1;
+        while (i<j){
+            if(s.charAt(i) != s.charAt(j)) {
+                return false ;
+            }
+            i++;
+            j--;
+        }
+        return true ;
     }
 }
