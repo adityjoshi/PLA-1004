ca6039db11352efc9397455b4da1ed99b5d96b46
Aditya Joshi <aditya03joshi@gmail.com>
2024-03-15 12:14:17 +0530

    java

---
+++ src/Algo/Palindrome.java
@@ -0,0 +1,11 @@
+package Algo;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
---
+++ src/Algo/PalindromeBitwise.java
@@ -0,0 +1,18 @@
+package Algo;
+
+public class PalindromeBitwise {
+    public static void main(String[] args) {
+        binary(9);
+        System.out.println(binary(9));
+    }
+    static int binary(int n) {
+        String s = "" ;
+        while(n>0) {
+            s = Integer.toBinaryString(n & 1) + s ;
+            n>>=1;
+        }
+        StringBuilder s1 = new StringBuilder(s);
+        s1 = s1.reverse();
+        return s.equals(s1.toString())?1:0;
+    }
+}
