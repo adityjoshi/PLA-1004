2ca3816a92fef554641b3527b5658b7666501c13
Aditya Joshi <aditya03joshi@gmail.com>
2024-03-27 12:30:08 +0530

    pla

---
+++ src/Algo/MaxEquilibrium.java
@@ -0,0 +1,19 @@
+/*
+{1,2,4,3,4,2,1}
+left right diff
+1 - 16 = 15
+3 - 14 = 11
+7 - 10 = 3
+10 - 7 = 3
+ */
+
+
+
+
+package Algo;
+
+public class MaxEquilibrium {
+    public static void main(String[] args) {
+
+    }
+}
